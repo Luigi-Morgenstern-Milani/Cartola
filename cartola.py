46398d2d7d901a79681e0817f4d5cb4bc3b85b62
Luigi Morgenstern Milani <luigimilani2003@outlook.com>
2025-08-16 20:18:29 -0300

    Create cartola.py

---
+++ cartola.py
@@ -0,0 +1,77 @@
+
+estatisticas_partida = [
+    
+    {"clube": "Corinthians", "nome": "Ángel Romero", "gols": 0, "assistencias": 0, "finalizacoes": 2, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 45, "passes_completos": 7, "passes_tentados": 8},
+    {"clube": "Corinthians", "nome": "Memphis", "gols": 0, "assistencias": 0, "finalizacoes": 3, "finalizacoes_no_alvo": 1, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 2, "minutos": 45, "passes_completos": 12, "passes_tentados": 17},
+    {"clube": "Corinthians", "nome": "Talles Magno", "gols": 0, "assistencias": 0, "finalizacoes": 2, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 45, "passes_completos": 8, "passes_tentados": 12},
+    {"clube": "Corinthians", "nome": "Yuri Alberto", "gols": 0, "assistencias": 0, "finalizacoes": 3, "finalizacoes_no_alvo": 2, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 45, "passes_completos": 4, "passes_tentados": 7},
+    {"clube": "Corinthians", "nome": "Dieguinho", "gols": 0, "assistencias": 0, "finalizacoes": 2, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 2, "minutos": 63, "passes_completos": 22, "passes_tentados": 30},
+    {"clube": "Corinthians", "nome": "André Carrillo", "gols": 1, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 1, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 27, "passes_completos": 29, "passes_tentados": 41},
+    {"clube": "Corinthians", "nome": "Breno", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 3, "minutos": 90, "passes_completos": 54, "passes_tentados": 63},
+    {"clube": "Corinthians", "nome": "Ryan Gustavo", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 3, "minutos": 45, "passes_completos": 20, "passes_tentados": 23},
+    {"clube": "Corinthians", "nome": "Rodrigo Garro", "gols": 0, "assistencias": 0, "finalizacoes": 3, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 45, "passes_completos": 26, "passes_tentados": 42},
+    {"clube": "Corinthians", "nome": "Charles Rigon Matos", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 3, "minutos": 90, "passes_completos": 44, "passes_tentados": 54},
+    {"clube": "Corinthians", "nome": "Fabrizio Angileri", "gols": 0, "assistencias": 0, "finalizacoes": 2, "finalizacoes_no_alvo": 1, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 2, "minutos": 90, "passes_completos": 75, "passes_tentados": 91},
+    {"clube": "Corinthians", "nome": "Cacá", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 3, "minutos": 90, "passes_completos": 81, "passes_tentados": 85},
+    {"clube": "Corinthians", "nome": "André Ramalho", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 4, "minutos": 90, "passes_completos": 94, "passes_tentados": 99},
+    {"clube": "Corinthians", "nome": "Félix Torres Caicedo", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 1, "minutos": 45, "passes_completos": 30, "passes_tentados": 32},
+    {"clube": "Corinthians", "nome": "Matheuzinho", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 45, "passes_completos": 46, "passes_tentados": 54},
+    {"clube": "Corinthians", "nome": "Hugo Souza", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 90, "passes_completos": 29, "passes_tentados": 32},
+    
+   
+    {"clube": "Fortaleza", "nome": "Deyverson", "gols": 0, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 56, "passes_completos": 14, "passes_tentados": 21},
+    {"clube": "Fortaleza", "nome": "Juan Martín Lucero", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 34, "passes_completos": 6, "passes_tentados": 9},
+    {"clube": "Fortaleza", "nome": "Breno", "gols": 1, "assistencias": 0, "finalizacoes": 2, "finalizacoes_no_alvo": 2, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 1, "minutos": 76, "passes_completos": 12, "passes_tentados": 16},
+    {"clube": "Fortaleza", "nome": "José Welison", "gols": 0, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 0, "cartao_amarelo": 1, "cartao_vermelho": 0, "desarmes": 0, "minutos": 14, "passes_completos": 7, "passes_tentados": 7},
+    {"clube": "Fortaleza", "nome": "Marinho", "gols": 0, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 2, "minutos": 76, "passes_completos": 10, "passes_tentados": 16},
+    {"clube": "Fortaleza", "nome": "José Herrera", "gols": 0, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 2, "minutos": 14, "passes_completos": 2, "passes_tentados": 2},
+    {"clube": "Fortaleza", "nome": "Lucca Prior", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 1, "minutos": 64, "passes_completos": 18, "passes_tentados": 23},
+    {"clube": "Fortaleza", "nome": "Leandro Emmanuel Martínez", "gols": 0, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 26, "passes_completos": 3, "passes_tentados": 3},
+    {"clube": "Fortaleza", "nome": "Matheus Pereira da Silva", "gols": 0, "assistencias": 1, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 90, "passes_completos": 39, "passes_tentados": 46},
+    {"clube": "Fortaleza", "nome": "Lucas Sasha", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 3, "minutos": 90, "passes_completos": 29, "passes_tentados": 33},
+    {"clube": "Fortaleza", "nome": "Diogo Barbosa", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 1, "minutos": 90, "passes_completos": 22, "passes_tentados": 25},
+    {"clube": "Fortaleza", "nome": "Gustavo Mancha", "gols": 0, "assistencias": 0, "finalizacoes": 1, "finalizacoes_no_alvo": 1, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 90, "passes_completos": 16, "passes_tentados": 21},
+    {"clube": "Fortaleza", "nome": "Benjamín Kuscevic", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 5, "minutos": 90, "passes_completos": 18, "passes_tentados": 28},
+    {"clube": "Fortaleza", "nome": "Eros Mancuso", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 64, "passes_completos": 27, "passes_tentados": 40},
+    {"clube": "Fortaleza", "nome": "Guilherme Tinga", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 2, "minutos": 26, "passes_completos": 4, "passes_tentados": 11},
+    {"clube": "Fortaleza", "nome": "Vinicius Silvestre", "gols": 0, "assistencias": 0, "finalizacoes": 0, "finalizacoes_no_alvo": 0, "cartao_amarelo": 0, "cartao_vermelho": 0, "desarmes": 0, "minutos": 90, "passes_completos": 19, "passes_tentados": 32}
+]
+pontuação=[]
+def calcular_pontuacao(estatisticas_partida):
+    for jogador in estatisticas_partida:
+        pontos = 0
+        passes_errados = jogador["passes_tentados"] - jogador["passes_completos"]
+        finalizacao_fora = jogador["finalizacoes"] - jogador["finalizacoes_no_alvo"]
+
+        pontos += jogador["gols"] * 8
+        pontos += jogador["assistencias"] * 5
+        pontos -= passes_errados * 0.1
+        pontos += finalizacao_fora * 0.8
+        pontos += jogador["finalizacoes_no_alvo"] * 1.2
+        pontos -= jogador["cartao_amarelo"] * 1
+        pontos -= jogador["cartao_vermelho"] * 3
+        pontos += jogador["desarmes"] * 1.2
+
+        if jogador["minutos"] >= 75:
+            pontos += pontos * 0.10
+
+        jogador["pontuacao"] = round(pontos, 2)
+        print(f"{jogador['nome']} ({jogador['clube']}) - Pontuação: {jogador['pontuacao']}")
+
+print("Pontuação dos jogadores:")
+calcular_pontuacao(estatisticas_partida)
+
+jogadores_ordenados = sorted(estatisticas_partida, key=lambda x: x["pontuacao"], reverse=True)
+print("Jogadores maiores pontuadores para menores:")
+for jogador in jogadores_ordenados:
+    print(f"{jogador['nome']} ({jogador['clube']}): {jogador['pontuacao']} pontos")
+
+print("Top 3 jogadores:")
+for i in range(3):
+    jogador = jogadores_ordenados[i]
+    print(f"{i+1}º: {jogador['nome']} ({jogador['clube']}) : {jogador['pontuacao']} pontos")
+
+print("Últimos 3 jogadores:")
+for i in range(1, 4):
+    jogador = jogadores_ordenados[-i]
+    print(f"{len(jogadores_ordenados)-i+1}º: {jogador['nome']} ({jogador['clube']}) :{jogador['pontuacao']} pontos")
